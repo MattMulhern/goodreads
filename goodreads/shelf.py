c133c1f44036ebf217394263b6fa2017b3de399e
Matt Mulhern <mmulhern@proofpoint.com>
2017-07-20 12:39:33 +0100

    Implementing shelf.books()
    Signed-off-by: Matt Mulhern <mmulhern@proofpoint.com>

--- goodreads/shelf.py
+++ goodreads/shelf.py
@@ -20,3 +20,16 @@ class GoodreadsShelf:
     @property
     def count(self):
         return int(self._shelf_dict['book_count']['#text'])
+
+    @property
+    def books(self, full=True, max_results=200):
+        books = {}
+        user = self._client.user()
+        req = self._client.request("/review/list/%s.xml" % user, {'shelf': self, 'per_page': max_results})
+        for req_book in req['books']['book']:
+            if full:
+                print("fetching {0}".format(req_book['title']))
+                books[req_book['title']] = self._client.book(book_id=req_book['id']['#text'])
+            else:
+                books[req_book['title']] = book.GoodreadsBook(client=self._client, book_dict=req_book)
+        return books
